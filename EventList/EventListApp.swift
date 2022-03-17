bb2d069212fe38450be4588893a3204fa4075deb
Adejuwon Ayodimeji <juwwarn28@gmail.com>
2022-03-17 10:49:43 +0100

    Initial Commit

---
+++ EventList/EventListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventListApp.swift
+//  EventList
+//
+//  Created by Ayodimeji Adejuwon on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
